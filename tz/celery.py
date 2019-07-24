5d1e23fe1b62b795364a14b89de5e2eee33e42d4
GysevOV <gysevov@yandex.ru>
2019-07-24 14:36:09 +0300

    make it better

--- tz/celery.py
+++ tz/celery.py
@@ -1,7 +1,8 @@
 import os
 import celery
-from tz import settings
+from django.conf import settings
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tz.settings')
 app_test = celery.Celery('tz')
 app_test.conf.update(BROKER_URL=os.environ['REDIS_URL'], CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
-app_test.autodiscover_tasks()
+app_test.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
