32c85f79adaed20ce66b34db0c0b5abaf251f297
GysevOV <gysevov@yandex.ru>
2019-07-24 13:56:46 +0300

    Update files, which use celery

--- tz/celery.py
+++ tz/celery.py
@@ -4,4 +4,4 @@ from tz import settings
 
 app_test = celery.Celery('tz')
 app_test.conf.update(BROKER_URL=os.environ['REDIS_URL'], CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
-app_test.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app_test.autodiscover_tasks()
